2d8ce8a4ccde007949a767f37f7cb15edbd54e12
Alaine <janelle_einsley_agero@dlsu.edu.ph>
2024-04-01 11:50:14 +0800

    feat: Comparative printing of values in Y and time

--- LBYARCH_Agero,Sales_x86-to-C_interface/stencil1DComputation_C_Caller.c
+++ LBYARCH_Agero,Sales_x86-to-C_interface/stencil1DComputation_C_Caller.c
@@ -6,42 +6,6 @@
 
 extern float stencil1DComputation(long long int offset, float X[]);
 
-int main() {
-    double n = pow(2,5);
-    float* X = (float*)malloc(n * sizeof(float));
-
-    for (int i = 0; i < n; i++) {
-        X[i] = (float)i+1;
-    }
-
-    int offset = 0;
-
-    int arrYSize = ((int)n - 7) + 1;
-    float* Y = (float*)malloc(arrYSize * sizeof(float));
-    
-    clock_t start, end;
-    double total;
-    start = clock();
-
-    for (int i = 0; i < arrYSize; i++) {
-        Y[i] = stencil1DComputation(offset, X);
-        offset++;
-    }
-    
-    end = clock();
-
-    printf("ASM + C. Length: %.0lf\nY[] = ", n);
-    for (int i = 0; i < arrYSize; i++) {
-        printf("%.3f\n ", Y[i]);
-    }
-
-    total = (double)(end - start) / CLOCKS_PER_SEC;
-
-    printf("Total time : %lf\n", total);
-
-    return 0;
-}
-
 void stencil1DComputationC(long long int arrYSize, float X[], float* Y) {
     long long int offset = 0;
 
@@ -57,4 +21,55 @@ void stencil1DComputationC(long long int arrYSize, float X[], float* Y) {
         offset++;
     }
 }
+
+int main() {
+    double n = pow(2,20);
+    float* X = (float*)malloc(n * sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        X[i] = (float)i+1;
+    }
+
+    int offset = 0;
+
+    int arrYSize = ((int)n - 7) + 1;
+    float* Y_ASM = (float*)malloc(arrYSize * sizeof(float));
+    
+    clock_t start_ASM, end_ASM;
+    double total_ASM;
+    start_ASM = clock();
+
+    for (int i = 0; i < arrYSize; i++) {
+        Y_ASM[i] = stencil1DComputation(offset, X);
+        offset++;
+    }
+    
+    end_ASM = clock();
+
+    clock_t start_C, end_C;
+    double total_C;
+    start_C = clock();
+    float* Y_C = (float*)malloc(arrYSize * sizeof(float));
+
+    stencil1DComputationC(arrYSize, X, Y_C);
+    end_C = clock();
+
+    //Comparative printing of output
+    //printf("ASM + C vs Pure C. Length: %.0lf\n", n);
+    //printf("Values of Y[]:\n");
+    //printf("ASM + C \t\tPure C\n");
+    //for (int i = 0; i < arrYSize; i++) {
+    //    printf("%.3f\t\t%.3f\n ", Y_ASM[i], Y_C[i]);
+    //}
+    
+    total_ASM = (double)(end_ASM - start_ASM) / CLOCKS_PER_SEC;
+    total_C = (double)(end_C - start_C) / CLOCKS_PER_SEC;
+
+    //comparative printing of time
+    printf("Time taken (in seconds): \n");
+    printf("ASM + C \tPure C\n");
+    printf("%.3f\t\t%.3f\n", total_ASM, total_C);
+
+    return 0;
 }
+
